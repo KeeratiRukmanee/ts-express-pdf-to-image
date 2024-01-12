632dda1d5dc87b4f94d6773358392dba0197056e
Keerati Rukmanee <keerati.ruk@aksorn.com>
2024-01-12 21:50:12 +0700

    - get thumb - get page

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import multer from 'multer'
 import cors from 'cors';
 import { v4 as uuidv4 } from 'uuid'
 import _ from 'lodash'
+import sharp from 'sharp';
+import gm from 'gm';
 
 const app = express();
 const PORT = 3000;
@@ -25,61 +27,147 @@ app.get('/', (req, res) => {
     res.send('Hello, TypeScript Express App!');
 });
 
+//// test node gm
+// app.get('/gm', (req, res) => {
+//     const GM = gm.subClass({ appPath: '/usr/bin/' });
+//     GM('output.jpg')
+//         .identify(function (err, data) {
+//             if (!err) console.log(data)
+//         });
+//     res.send('Hello, TypeScript Express App!');
+// });
+
+app.get('/page/:workBookId/:page', async (req, res) => {
+
+    const workBookId = req.params.workBookId
+    const page = parseInt(req.params.page)
+
+    const tmpDir: string = `${__dirname}/storage/${workBookId}/tmp`
+    const files = fs.readdirSync(tmpDir)
+
+    const fileName = files[page - 1]
+    const filePath = `${tmpDir}/${fileName}`
+
+    // sharp
+    const image = sharp(filePath)
+    const metadata = await image.metadata()
+
+    const resp: ResponseDTO = {
+        requestId: uuidv4(),
+        success: true,
+        message: "get image success",
+        data: {
+            file: `/static/${workBookId}/tmp/${fileName}`,
+            info: metadata,
+        }
+    }
+
+    res.json(resp)
+});
+
+app.get('/thumb/:workBookId/:page/:row', async (req, res) => {
+
+    const workBookId = req.params.workBookId
+    const page = parseInt(req.params.page)
+    const row = parseInt(req.params.row)
+
+    const tmpDir: string = `${__dirname}/storage/${workBookId}/thumbnail`
+    const files = fs.readdirSync(tmpDir)
+
+    const start_index = (page - 1) * row
+    const end_index = start_index + row
+    const resPage = files.slice(start_index, end_index);
+
+    const resp: ResponseDTO = {
+        requestId: uuidv4(),
+        success: true,
+        message: "get image thumbnail success",
+        data: {
+            page: page,
+            row: row,
+            total: files.length,
+            files: resPage.map((page => { return `/static/${workBookId}/thumbnail/${page}` })),
+        }
+    }
+
+    res.json(resp)
+});
+
 app.post('/convert', upload.single('file'), (req, res) => {
 
+    // validate if form have file
     if (!req.file) {
         return res.status(400).json({ error: 'No file uploaded' });
     }
 
     const file = req.file
 
-    const projectId = uuidv4()
+    // generate workBook id
+    const workBookId = uuidv4()
 
-    const newDir: string = `${__dirname}/storage/${projectId}`
-    console.log(file.originalname.replace(/ /g, "_"))
-    // const filePath = path.join(newDir, file.originalname)
-    const filePath = path.join(newDir, file.originalname.replace(/ /g, "_"))
+    // workBook directory
+    const newDir: string = `${__dirname}/storage/${workBookId}`
 
+    // origin PDF file directory
+    const originDir: string = `${__dirname}/storage/${workBookId}/origin`
+
+    // tmp directory
+    const tmpDir: string = `${__dirname}/storage/${workBookId}/tmp`
+
+    // thumbnail directory
+    const thumpDir: string = `${__dirname}/storage/${workBookId}/thumbnail`
+
+    // path to save origin PDF file // replace space with _
+    const filePath = path.join(originDir, file.originalname.replace(/ /g, "_"))
+
+    // create directory 
     fs.mkdirSync(newDir)
+    fs.mkdirSync(originDir)
+    fs.mkdirSync(tmpDir)
+    fs.mkdirSync(thumpDir)
+
+    // write origin PDF file from buffer
     fs.writeFile(filePath, file.buffer, (error) => {
         if (error) {
             res.json({ error: error })
         }
     })
 
-    // gm convert -density 200 -quality 50%  -background white kme_RE.pdf[11] magick12-50.jpg
-    exec(`gm convert -density 200 -quality 75% ${filePath} +adjoin ${newDir}/${projectId}-%03d.jpg`, (error, stdout, stderr) => {
+    // convert PDF file to multiple image files (JPG)
+    exec(`gm convert -density 200 -quality 75% ${filePath} +adjoin ${tmpDir}/${workBookId}-%03d.jpg`, (error, stdout, stderr) => {
         if (error) {
             // node couldn't execute the command
-
             //TODO delete file and folder when error
-            // fs.rm(newDir, { recursive: true, force: true }, err => {
-            //     if (err) {
-            //         throw err;
-            //     }
-            // })
-
             res.json({ error: error })
         }
 
-        const files = fs.readdirSync(newDir)
+        // list all files in tmp directory
+        const files = fs.readdirSync(tmpDir)
 
         let allFiles: String[] = []
 
-        for (let i = 0; i < (files.length - 1); i++) {
+        for (let i = 0; i < files.length; i++) {
+
+            // %03d 001 format
             const fileIdx = _.padStart(i.toString(), 3, '0')
-            allFiles.push(`/static/${projectId}/${projectId}-${fileIdx}.jpg`);
+
+            // generate thumbnail files
+            exec(`gm convert ${tmpDir}/${workBookId}-${fileIdx}.jpg -thumbnail 200x ${thumpDir}/${workBookId}-thumbnail-${fileIdx}.jpg`, (error, stdout, stderr) => {
+                if (error) {
+                    // node couldn't execute the command
+                    //TODO delete file and folder when error
+                    res.json({ error: error })
+                }
+            })
         }
 
-        let pages = files.length - 1
 
         const resp: ResponseDTO = {
-            requestId: projectId,
+            requestId: workBookId,
             success: true,
             message: "upload and convert to image success",
             data: {
-                pages: pages,
-                files: allFiles,
+                pages: files.length,
             }
         }
 
